d866f214336cb0bd690d147c9af17b707b2afc7c
mergenannmrdv@gmail.com <mergenannmrdv@gmail.com>
2023-02-24 18:34:54 +0300

    12.gün 1.ödev 1.commit

---
+++ Business/ValidationRules/FluentValidation/BrandValidator.cs
@@ -0,0 +1,25 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class BrandValidator:AbstractValidator<Brand>
+    {
+        public BrandValidator()
+        {
+            RuleFor(p => p.Name).NotEmpty();
+            RuleFor(p => p.Name).MaximumLength(2);
+            RuleFor(p => p.Name).Must(StartWithB).WithMessage("B harfi ile başlaması gerekiyor");
+
+            
+        }
+
+        private bool StartWithB(string arg)
+        {
+            return arg.StartsWith("B");
+        }
+    }
+}
